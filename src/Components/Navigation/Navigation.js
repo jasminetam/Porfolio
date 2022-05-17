f3ba5ebaab295ba4b716320c160ea91bc55e95b9
jasminetam <jasminetam94@gmail.com>
2022-05-17 21:03:52 +0100

    adding icons

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import "./Navigation.css";
-
+import { BsGithub } from 'react-icons/bs';
+import { SiIndeed } from 'react-icons/si';
+import { FiTwitter } from 'react-icons/fi';
 
 export default function Navigation() {
     return (
         <div className="navigation">
                 <div className="topLeft">
-                <i class="fa-brands fa-facebook"></i>
+                    <BsGithub size="30px" />
+                    <SiIndeed size="30px" />
+                    <FiTwitter size="30px" />
                 </div>
-                <div className="topCenter"></div>
-                <div className="topRight"></div>
+                <div className="topCenter">
+
+                </div>
+                <div className="topRight">
+                    <p>About</p>
+                    <p>Blog</p>
+                    <p>Contact</p>
+                </div>
         </div>
     )
 };
